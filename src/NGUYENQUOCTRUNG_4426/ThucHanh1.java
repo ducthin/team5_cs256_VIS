3cccd25890caad26c6a5bb2ce250fab231b2a00b
Nguyen Quoc Trung <trungnguyen1422004@gmail.com>
2024-02-28 22:05:29 +0700

    Bai Thuc Hanh 1.

---
+++ src/NGUYENQUOCTRUNG_4426/ThucHanh1.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package NGUYENQUOCTRUNG_4426;
+
+/**
+ *
+ * @author trung
+ */
+public class ThucHanh1 {
+    public static void main(String[] args) {
+        Menu frm = new Menu(); //Create New Form
+        frm.setLocationRelativeTo(null); //form -> center
+        frm.setVisible(true);  //Display
+    }
+}
